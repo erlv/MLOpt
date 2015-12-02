0f53f0936f745bb5bdab5ccdc5430c4c4c3379fa
Kun Ling <kunling@lingcc.com>
2015-12-01 22:17:07 -0800

    SSE code start

--- include/MatrixMul/MMSSE.hpp
+++ include/MatrixMul/MMSSE.hpp
@@ -5,12 +5,13 @@
 
 using namespace std;
 
-class MMInt8SSE: public MMBase<int8_t> {
+class MMInt8SSE: public MMScalar<int8_t> {
 private:
   string _name;
 public:
-  MMInt8SSE (int M, int N, int K, string postfix) {
-    
+  MMInt8SSE (int M, int N, int K, string postfix)
+    :MMScalar<int8_t>(M,N,K,postfix) {
+      _name = "MMInt8SSE<" + postfix + ">";
   }
-
-}
+  void mmSSE(TYPE_T* matA, TYPE_T* matB, TYPE_T* matC, int M, int N, int K);
+};
--- lib/MatrixMul/MMSSE.cpp
+++ lib/MatrixMul/MMSSE.cpp
@@ -1,1 +1,9 @@
 #include "MatrixMul/MMSSE.hpp"
+
+
+void MMInt8SSE::MMSSE(TYPE_T* matA, TYPE_T* matB, TYPE_T* matC, int M,
+                      int N, int K) {
+
+
+
+}
